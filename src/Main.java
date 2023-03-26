b79d8d658948e97d47396b3a360f0bd0fabf0395
lucas.gentile95@gmail.com <lucas.gentile95@gmail.com>
2023-03-26 18:56:27 -0300

    Upando as primeiras classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import br.com.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Linguagem de Programação Orientada a Objetos");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Python");
+        curso2.setDescricao("Linguagem multiparadigma");
+        curso2.setCargaHoraria(8);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+
     }
 }
